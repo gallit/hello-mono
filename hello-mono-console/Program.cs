a4dcd6a4d3e0f3631bc2ae00d9accfeb47bdf038
gallit <gallit@gmail.com>
2015-06-09 16:07:32 +0200

    Console update

--- hello-mono-console/Program.cs
+++ hello-mono-console/Program.cs
@@ -7,6 +7,18 @@ namespace hellomonoconsole
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+			hello6 ();
+		}
+
+		/// <summary>
+		/// C# 6 string interpolation
+		/// </summary>
+		private static void hello6()
+		{
+			var data = new { Machine = Environment.MachineName };
+			var s = $"C# Hello {data.Machine}, we're on {DateTime.Today:D}";
+			Console.WriteLine (s);
+
 		}
 	}
 }
